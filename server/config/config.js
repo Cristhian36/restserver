128bc72d7ed522df21dac95decdbdf72b5545d03
Cristhian36 <cristhian.b.g180@gmail.com>
2019-04-18 10:05:59 -0400

    Env Mongo_URL mal

--- server/config/config.js
+++ server/config/config.js
@@ -32,7 +32,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URL;
+    urlDB = 'mongodb://p0027:p0027@cluster0-shard-00-00-qpls7.mongodb.net:27017,cluster0-shard-00-01-qpls7.mongodb.net:27017,cluster0-shard-00-02-qpls7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 }
 
 
