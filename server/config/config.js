29758698453b8b08b584b3b334efd35ef6bc5df6
Cristhian36 <cristhian.b.g180@gmail.com>
2019-04-19 18:12:52 -0400

    Guardar backup V.0.02

--- server/config/config.js
+++ server/config/config.js
@@ -31,18 +31,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-<<<<<<< HEAD
 
-urlDB = 'mongodb://p0027:p0027@cluster0-shard-00-00-qpls7.mongodb.net:27017,cluster0-shard-00-01-qpls7.mongodb.net:27017,cluster0-shard-00-02-qpls7.mongodb.net:27017/test?ssl=true';
-
-=======
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     // urlDB = 'mongodb+srv://p0027:p0027@cluster0-qpls7.mongodb.net/test?retryWrites=true';
     urlDB = process.env.MONGO_URI;
 }
->>>>>>> 23b4a6baac00913c404000cbf02d287d9e5c4a67
+
 
 
 process.env.URLDB = urlDB;
