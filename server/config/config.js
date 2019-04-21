0553888104c5e4494b88999736c1e9654e5d2f58
Cristhian36 <cristhian.b.g180@gmail.com>
2019-04-20 22:33:06 -0400

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -28,17 +28,19 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 ////////////////////
 
 
-
 let urlDB;
 
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    // urlDB = 'mongodb+srv://p0027:p0027@cluster0-qpls7.mongodb.net/test?retryWrites=true';
     urlDB = process.env.MONGO_URI;
 }
 
-
-
 process.env.URLDB = urlDB;
+
+///////////////////////////
+//// Client ID////
+////////////////////
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '828613885048-hgk9ji8mtdlrqghsouu9ug9n9seq25qk.apps.googleusercontent.com';
